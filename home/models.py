892a46b4c687853390260b540e017e8210ffcce5
sarahgoodwin93 <>
2023-12-30 17:22:47 +0000

    (FEAT-HOME): add class Meta and a model method to comment model

--- home/models.py
+++ home/models.py
@@ -19,3 +19,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
