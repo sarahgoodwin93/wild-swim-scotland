2b9bf4fac0dd284f94ea3e8876762a2c8186336e
sarahgoodwin93 <>
2023-12-30 15:50:19 +0000

    (SETUP): add secret key

--- wildswim/settings.py
+++ wildswim/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6+nztkj95$2n@@f=c2&t6k&_bmduqt$%22*^li^1qzthydz^8='  # noqa
+SECRET_KEY = os.environ.get("SECRET_KEY")  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
