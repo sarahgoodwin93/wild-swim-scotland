7ec26b43340410328f53e042ec5d26234e7d3ed6
sarahgoodwin93 <>
2024-01-19 07:55:18 +0000

    (FEAT/delete swim) fix case senstive href for delete swim - now working!

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ from .views import (SwimList, AddSwimView, SwimDeleteView)
 urlpatterns = [
     path('', views.SwimList.as_view(), name='home'),
     path("swim/add", AddSwimView.as_view(), name="add_swim"),
-    path("delete/<int:id>/", views.SwimDeleteView.as_view(), name="delete_swim"),  # noqa
+    path("article/delete/<int:pk>/", SwimDeleteView.as_view(), name="delete_swim"),  # noqa
 ]
