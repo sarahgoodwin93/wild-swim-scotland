09fe0c38ab9f7efc77a88a1c82884ae6bef566b0
sarahgoodwin93 <>
2024-01-19 08:08:30 +0000

    (FEAT/urls.py) add Edit Swim URL path

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.SwimList.as_view(), name='home'),
     path("swim/add", AddSwimView.as_view(), name="add_swim"),
     path("article/delete/<int:pk>/", SwimDeleteView.as_view(), name="delete_swim"),  # noqa
+    path("article/edit/<int:pk>/", EditSwimView.as_view(), name="edit_swim"),  # noqa
 ]
