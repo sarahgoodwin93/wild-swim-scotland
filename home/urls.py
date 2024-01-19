d846e630c57d9084733206c2744c0f336fc7cd6e
sarahgoodwin93 <>
2024-01-19 07:41:02 +0000

    (FEAT/Delete Swim): continue working on delete swim by adding href to html

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,10 @@
 from . import views
 from django.urls import path
-from .views import (SwimList, AddSwimView)
+from .views import (SwimList, AddSwimView, SwimDeleteView)
 
 
 urlpatterns = [
     path('', views.SwimList.as_view(), name='home'),
     path("swim/add", AddSwimView.as_view(), name="add_swim"),
-    path("delete/<int:pk>/", views.SwimDeleteView.as_view(), name="delete_swim"),  # noqa
+    path("delete/<int:id>/", views.SwimDeleteView.as_view(), name="delete_swim"),  # noqa
 ]
