337564eeef81f9706dc1fe377a57657852197010
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-01-31 19:16:40 +0000

    (FIX/reviews) remove html templates and review nav link

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SwimPosts, Review
+from .models import SwimPosts
 
 
 @admin.register(SwimPosts)
@@ -7,8 +7,3 @@ class SwimAdmin(admin.ModelAdmin):
     swim_post_list = ("title", "description", "created_on", "contributer")
     search_fields = ("title", "description", "created_on", "contributer")
 
-
-@admin.register(Review)
-class RegisterAdmin(admin.ModelAdmin):
-    swim_post_list = ("title", "review", "creator", "created_on")
-    search_fields = ("title", "review", "creator", "created_on")
