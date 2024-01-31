ac36af9540ba4e0564519fa44d6c01f63dec5787
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-01-31 08:56:17 +0000

    (STATIC) python manage.py collectstatic

---
+++ staticfiles/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let joinButton = document.querySelectorAll(".join-swim-btn");
+
+    joinButton.forEach(function(button) {
+        button.addEventListener("click", function() {
+            let joined = button.classList.contains("joined");
+
+            if(!joined) {
+                button.textContent = "Swim Joined";
+                button.classList.add("joined");
+                button.style.backgroundColor = "#337ab7";
+            } else {
+                button.textContent = "Join this swim";
+                button.classList.remove("joined");
+                button.style.backgroundColor = "";
+            }
+        });
+    });
+});
